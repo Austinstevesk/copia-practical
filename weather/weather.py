0b6ec9f8340cb950056b672e261944a838826347
Austinstevesk <austinstevesk@gmail.com>
2022-03-20 10:52:46 +0300

    finalized solving the weather problem

--- weather/weather.py
+++ weather/weather.py
@@ -2,7 +2,7 @@ with  open('weather.dat') as f:
     lines = f.read().splitlines() #using this ensures we don't include line separators (\n) which come with f.readlines()
     # print(lines)
 
-    spread = list()
+    spread, day = list(), list(),
 
     #We start at index 2 where actual data starts and we get to the second last
     for i in range(2, len(lines)-1): 
@@ -19,29 +19,27 @@ with  open('weather.dat') as f:
         if '*' not in cols[1] and '*' not in cols[2]:
             #get the difference
             diff = int(cols[1]) - int(cols[2])
-            spread.append({cols[0]: (diff)})
+            spread.append(diff)
+            day += cols[0]
 
         #Cater for values with asterics in the MxT column
         elif '*' in cols[1]:
             cols[1] = cols[1][:-1]
             #print(cols[1])
             diff = int(cols[1]) - int(cols[2])
-            spread.append({cols[0]: (diff)})
+            spread.append(diff)
+            day += cols[0]
 
         #Cater for values with asterics in the MnT column    
         elif '*' in cols[2]:
             cols[2] = cols[2][:-1]
             diff = int(cols[1]) - int(cols[2])
-            spread.append({cols[0]: (diff)})
+            spread.append(diff)
+            day += cols[0]
 
 
-#print(spread)
-vals, keys = list(), list()
-for i in spread:
-    vals += i.values()
-    keys += i.keys()
-max_diff = max(vals)
+max_diff = max(spread)
 #print(max_diff)
-idx = vals.index(54)
-res = keys[8] + " " + str(max_diff)
+idx = spread.index(max_diff)
+res = day[idx] + " " + str(max_diff)
 print(res)
